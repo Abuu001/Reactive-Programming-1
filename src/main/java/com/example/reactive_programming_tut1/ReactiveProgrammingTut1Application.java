bcc795c72a5dde6fcce36ac6a41cbb5a17353eb4
Abuu001 <abulugz@gmail.com>
2022-06-29 12:49:05 +0300

    activate debugging for reactive

--- src/main/java/com/example/reactive_programming_tut1/ReactiveProgrammingTut1Application.java
+++ src/main/java/com/example/reactive_programming_tut1/ReactiveProgrammingTut1Application.java
@@ -2,11 +2,13 @@ package com.example.reactive_programming_tut1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.tools.agent.ReactorDebugAgent;
 
 @SpringBootApplication
 public class ReactiveProgrammingTut1Application {
 
     public static void main(String[] args) {
+        ReactorDebugAgent.init();  // this is for initializing debugging tools for project reactor
         SpringApplication.run(ReactiveProgrammingTut1Application.class, args);
     }
 
